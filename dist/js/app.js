7db84f1f432093d77c311b77808acc83d40ce79d
jming <jming@college.harvard.edu>
2016-02-09 20:27:32 +0530

    random data changes

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,33 +7,36 @@
 //     });
 //   }
 
+var all_circles = [];
+var all_colors = ['red', 'orange', 'yellow', 'green'];
+
 var villages = {
 
-	dadenggre: {
+	Dadenggre: {
 		center: {lat: 25.721316, lng: 90.194303},
 		rainfall: 1.5
 	},
-	rongram: {
+	Rongram: {
 		center: {lat: 25.593040, lng: 90.263311},
 		rainfall: 2.0
 	},
-	hatsingimari: {
+	Hatsingimari: {
 		center: {lat: 25.711882, lng: 89.898015},
 		rainfall: 1.2
 	},
-	jongchipara: {
+	Jongchipara: {
 		center: {lat: 25.875096, lng: 90.318242},
 		rainfall: 0.9
 	},
-	tikrikilla: {
+	Tikrikilla: {
 		center: {lat: 25.917099, lng: 90.149328},
 		rainfall: 1.8
 	},
-	mankachar: {
+	Mankachar: {
 		center: {lat: 25.538532, lng: 89.858190},
 		rainfall: 1.7
 	},
-	dingok: {
+	Dingok: {
 		center: {lat: 25.873860, lng: 90.483037},
 		rainfall: 2.5
 	}
@@ -57,19 +60,45 @@ var villages = {
 };
 
 function circleColor(rainfall) {
-	if (rainfall >= 2.5) {
-		return 'red'
-	} else if (rainfall >= 2.0 && rainfall < 2.5) {
-		return 'orange'
-	} else if (rainfall >= 1.5 && rainfall < 2.0) {
-		return 'yellow'
-	} else {
-		return 'green'
-	}
+	return all_colors[Math.floor(Math.random() * all_colors.length)];
+	// if (rainfall >= 2.5) {
+	// 	return 'red'
+	// } else if (rainfall >= 2.0 && rainfall < 2.5) {
+	// 	return 'orange'
+	// } else if (rainfall >= 1.5 && rainfall < 2.0) {
+	// 	return 'yellow'
+	// } else {
+	// 	return 'green'
+	// }
+}
+
+function randrange(min, max, dec) {
+	// return Math.floor(Math.random() * max)
+	return (Math.random() * (max - min) + min).toFixed(dec);
+}
+
+function randint(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function circleSize(rainfall) {
 	return Math.pow(10,3)*5;
+}
+
+function updateVillage(event) {
+	$('#village-name').text($(this)[0].data);
+
+	$('#flood-val').text(randint(0,100) + '%');
+	$('#rainfall-val').text(randrange(0,3,1) + ' mm ('+randint(0,100) + '%)');
+	$('#windspeed-val').text(randint(0,20) + ' km/h');
+	$('#maxtemp-val').text(randint(0,100) + ' C');
+	$('#mintemp-val').text(randint(0,100) + ' C');
+	$('#humidity-val').text(randint(0,100) + '%');
+
+	// console.log(event);
+	// console.log($(this)[0].data);
+	// console.log(event.data);
+	// console.log(data);
 }
 
 function initMap() {
@@ -85,17 +114,22 @@ function initMap() {
   for (var village in villages) {
     // Add the circle for this city to the map.
     var villageCircle = new google.maps.Circle({
-      strokeColor: circleColor(villages[village].rainfall),
+      // strokeColor: circleColor(villages[village].rainfall),
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: circleColor(villages[village].rainfall),
+      // fillColor: circleColor(),
       fillOpacity: 0.35,
       map: map,
       center: villages[village].center,
-      radius: circleSize(villages[village].rainfall)
+      radius: circleSize(villages[village].rainfall),
+      data: village
       // raidius: 
       // radius: Math.sqrt(villages[village]*1000000) * 100
     });
+    villageCircle.addListener('click', updateVillage);
+    // all_circles.push({circle: villageCircle, village: village});
+    all_circles.push(villageCircle);
 		// var marker = new google.maps.Marker({
 	 //    position: villages[village].center,
 	 //    map: map,
@@ -103,3 +137,9 @@ function initMap() {
 	 //  });
   }
 }
+
+$('#display-param').change(function() {
+	for (circle of all_circles) {
+		circle.setOptions({fillColor: circleColor(0)});
+	}
+})
