0d359fb5c4007af3c12deac33994aff2241a5c1d
jming <jming@college.harvard.edu>
2016-03-14 23:15:34 +0530

    revamp google maps layout

--- dist/js/app.js
+++ dist/js/app.js
@@ -101,23 +101,150 @@ function updateVillage(event) {
 	// console.log(data);
 }
 
+function controlLine(ui, type, params) {
+
+	var line = document.createElement('div');
+	line.style.color = 'rgb(25,25,25)';
+  line.style.fontFamily = 'Roboto,Arial,sans-serif';
+  line.style.fontSize = '16px';
+  line.style.lineHeight = (type == 'legendLine') ? '24px' : '38px';
+  line.style.paddingLeft = (type == 'legendLine') ? '20px' : '10px';
+  line.style.paddingRight = (type == 'legendLine') ? '20px' : '10px';
+  
+  // line.style.paddingTop = '5px';
+  // line.style.paddingBottom = '5px';
+  if (type == 'legendLine') {
+  	if (params.color == 'red') {
+  	line.style.paddingTop = '10px';
+	  }
+	  if (params.color == 'green') {
+	  	line.style.paddingBottom = '10px';
+	  }
+  	line.innerHTML = '<svg height="20" width="30"><circle cx="10" cy="10" r="8" stroke='
+  	+ params.color + ' stroke-width="3" fill=' + params.color + ' /></svg>' + params.text;
+
+  }
+  else {
+  	line.innerHTML = params.text;
+  }
+  ui.appendChild(line);
+
+}
+
+function controlTable(controlDiv, params) {
+	var table = document.createElement('table');
+	table.style.color = 'rgb(25,25,25)';
+  table.style.fontFamily = 'Roboto,Arial,sans-serif';
+  table.style.fontSize = '14px';
+  table.style.paddingLeft = '10px';
+  table.style.paddingRight = '10px';
+  // table.style.paddingTop = '20px';
+  table.style.paddingBottom = '10px';
+
+
+	for (var param of params) {
+		// console.log(param, param.name, param.value)
+		var tr = document.createElement('tr');
+		var td1  = document.createElement('td');
+		var td2 = document.createElement('tr');
+		var text1 = document.createTextNode(param.name);
+		var text2 = document.createTextNode(param.value);
+		td1.appendChild(text1);
+		td2.appendChild(text2);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		table.appendChild(tr);
+	}
+
+	controlDiv.appendChild(table);
+}
+
+function createControl(controlDiv, map, controlType, text) {
+	// Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginRight = '22px';
+  controlUI.style.marginTop = '22px';
+  controlUI.style.marginLeft = (controlType == 'measurement') ? '0px' : '22px';
+  controlUI.style.marginBottom = '22px';
+
+  controlUI.style.textAlign = (controlType == 'legend') ? 'left' : 'center';
+  controlUI.title = '';
+  controlDiv.appendChild(controlUI);
+
+  if (controlType == 'legend') {
+
+  	var legendLine1 = new controlLine(controlUI, 
+  		'legendLine', {'text': 'Very high', 'color': 'red'});
+  	var legendLine2 = new controlLine(controlUI, 
+  		'legendLine', {'text': 'High', 'color': 'orange'});
+  	var legendLine3 = new controlLine(controlUI, 
+  		'legendLine', {'text': 'Average', 'color': 'yellow'});
+  	var legendLine4 = new controlLine(controlUI, 
+  		'legendLine', {'text': 'Low', 'color': 'green'});
+	  // var legendLine1 = new legendLine(controlUI, 'Very High', 'red');
+	  // var legendLine2 = new legendLine(controlUI, 'High', 'orange');
+	  // var legendLine3 = new legendLine(controlUI, 'Average', 'yellow');
+	  // var legendLine4 = new legendLine(controlUI, 'Low', 'green');
+
+  }
+  else if (controlType == 'details') {
+
+  	var detailsLine = new controlLine(controlUI, 'detailLine', {'text': 'Dadenggre'})
+  	var detailsTable = new controlTable(controlUI, [
+  		{'name': 'Chance of flood', 'value': '40%'},
+  		{'name': 'Rainfall', 'value': '1.5 mm (50%)'},
+  		{'name': 'Windspeed', 'value': '13 km/h'},
+  		{'name': 'Max temp', 'value': '30 C'}, 
+  		{'name': 'Min temp', 'value': '22 C'},
+  		{'name': 'Humidity', 'value': '48%'}
+  	]);
+  	// controlUI.appendChild(detailsTable);
+
+  }
+  else {
+
+  	var generalLine = new controlLine(controlUI, 'general', {'text': text});
+
+  }
+
+  controlUI.addEventListener('click', function() {
+    // map.setCenter(chicago);
+    // alert('clicked');
+  });
+}
+
 function initMap() {
   // Create the map.
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: {lat: 25.721316, lng: 90.194303},
-    mapTypeId: google.maps.MapTypeId.TERRAIN
+    mapTypeId: google.maps.MapTypeId.TERRAIN,
+    mapTypeControl: false,
+	  zoomControl: true,
+	  zoomControlOptions: {
+	  	position: google.maps.ControlPosition.LEFT_BOTTOM
+	  },
+	  scaleControl: true,
+	  streetViewControl: false,
+	  fullScreenControl: true
   });
+
 
   // Construct the circle for each value in citymap.
   // Note: We scale the area of the circle based on the population.
   for (var village in villages) {
+  	var villageColor = circleColor(villages[village].rainfall);
     // Add the circle for this city to the map.
     var villageCircle = new google.maps.Circle({
-      // strokeColor: circleColor(villages[village].rainfall),
+      strokeColor: villageColor,
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: circleColor(villages[village].rainfall),
+      fillColor: villageColor,
       // fillColor: circleColor(),
       fillOpacity: 0.35,
       map: map,
@@ -136,6 +263,34 @@ function initMap() {
 	 //    title: 'Hello World!'
 	 //  });
   }
+  // Create the DIV to hold the control and call the CenterControl()
+        // constructor passing in this DIV.
+	var downloadDiv = document.createElement('div');
+	var downloadControl = new createControl(downloadDiv, map, 'download', 'Download Data');
+	downloadDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(downloadDiv);
+
+  var legendDiv = document.createElement('div');
+	var legend = new createControl(legendDiv, map, 'legend', '');
+	legendDiv.index = 1;
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legendDiv);
+
+  var paramsDiv = document.createElement('div');
+  var paramsControl = new createControl(paramsDiv, map, 'params', '24 hours');
+  paramsDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(paramsDiv);
+
+  var measurementDiv = document.createElement('div');
+  var measureControl = new createControl(measurementDiv, map, 'measurement', 'Rainfall');
+  measurementDiv.index = 1;
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(measurementDiv);
+
+  var detailsDiv = document.createElement('div');
+  var detailControl = new createControl(detailsDiv, map, 'details', '');
+  detailsDiv.index = 1;
+  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(detailsDiv);
+
+  // map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('legend'));
 }
 
 $('#display-param').change(function() {
